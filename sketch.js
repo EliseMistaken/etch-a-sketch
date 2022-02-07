24f292e9f3887340fd2e20f95884ef9f0049cfb9
EliseMistaken <lmayo5678@gmail.com>
2022-02-07 13:56:20 -0800

    Added resizing button, and gradually darkening divs

--- sketch.js
+++ sketch.js
@@ -2,13 +2,15 @@
 
 function generateBoard(squares=16){
     const container = document.querySelector('.container');
+    container.innerHTML = '';
     for (let i = 0; i < squares; i++){
         const row = document.createElement('div');
         row.classList.add('row');
         for (let x = 0; x < squares; x++){
             const square = document.createElement('div');
             square.classList.add('square');
-            square.style.height = `${1000/squares}px`;
+            square.style.height = `${window.innerHeight/(squares+1)}px`;
+            square.style.backgroundColor = 'rgb(255,255,255)';
             row.appendChild(square);
             square.addEventListener('mouseenter', rgbColor);
         }
@@ -25,8 +27,24 @@ function rgbColor(e){
     e.target.style.backgroundColor =  `rgb(${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)})`
 }
 
-function resizeBoard(){
-    let size = parseInt(prompt('How large would you like the '))
+function darkenColor(e){
+    const colorCSS = e.target.style.backgroundColor;
+    const color = colorCSS.substring(4, colorCSS.length-1);
+    console.log(color);
+    const parsedColor = parseInt(color.split(',')[0]);
+    console.log(parsedColor);
+    e.target.style.backgroundColor = `rgb(${parsedColor - 10}, ${parsedColor - 10}, ${parsedColor - 10})`;
 }
 
+function resizeBoard(){
+    let size = parseInt(prompt('How many squares would you like on each side? 0 < x < 100', '16'));
+    if(size && size > 0 && size < 100);
+    generateBoard(size);
+    
+}
+
+function clearBoard() {
+    const container = document.querySelector('.container');
+
+}
 generateBoard();
